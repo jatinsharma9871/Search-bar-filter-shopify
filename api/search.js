7fbc505555d93779535283204937f9f0dc0e238f
jatin sharma <jatinsharma9871@gmail.com>
2025-07-15 17:08:53 +0530

    test push

--- api/search.js
+++ api/search.js
@@ -1,37 +1,49 @@
+// api/search.js
 export default async function handler(req, res) {
   const { q } = req.query;
 
+  console.log("🔍 Query received:", q);
   const shop = process.env.SHOPIFY_SHOP;
   const token = process.env.SHOPIFY_ADMIN_TOKEN;
 
-  const response = await fetch(`https://${shop}/admin/api/2023-10/graphql.json`, {
-    method: "POST",
-    headers: {
-      "X-Shopify-Access-Token": token,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      query: `
-        {
-          products(first: 250, query: "${q}*", sortKey: TITLE) {
-            edges {
-              node {
-                id
-                handle
-                title
-                vendor
-                productType
-                tags
-                onlineStoreUrl
-              }
-            }
+  if (!shop || !token) {
+    return res.status(500).json({ error: "Missing env variables" });
+  }
+
+  try {
+    const response = await fetch(`https://${shop}/admin/api/2023-10/graphql.json`, {
+      method: "POST",
+      headers: {
+        "X-Shopify-Access-Token": token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+  query: `
+    {
+      products(first: 100, query: "title:*${q}* OR product_type:*${q}* OR tag:*${q}* OR vendor:*${q}*") {
+        edges {
+          node {
+            id
+            handle
+            title
+            vendor
+            productType
+            tags
+            onlineStoreUrl
           }
         }
-      `
-    })
-  });
+      }
+    }
+  `
+})
 
-  const data = await response.json();
-  const products = data.data.products.edges.map(edge => edge.node);
-  res.status(200).json({ products });
+    });
+
+    const json = await response.json();
+    const products = json.data.products.edges.map(edge => edge.node);
+    res.status(200).json({ products });
+  } catch (error) {
+    console.error("❌ Error calling Shopify API", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
