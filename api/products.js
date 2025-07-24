c1eb19cea0a2434a6c86cab6c942546bfc2856f9
jatin sharma <jatinsharma9871@gmail.com>
2025-07-24 12:41:08 +0530

    puch

--- api/products.js
+++ api/products.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     const gqlQuery = {
       query: `
         {
-          products(first: 250, query: "${query}*") {
+          products(first: 150, query: "${query}*") {
             edges {
               node {
                 id
