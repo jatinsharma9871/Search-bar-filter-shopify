514cb8dc9e0b95e2f644c3386943fa83528d405c
jatin sharma <jatinsharma9871@gmail.com>
2025-07-24 17:25:16 +0530

    final commit

--- api/products.js
+++ api/products.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     const gqlQuery = {
       query: `
         {
-          products(first: 100, query: "${query}*") {
+          products(first: 250, query: "${query}*") {
             edges {
               node {
                 id
