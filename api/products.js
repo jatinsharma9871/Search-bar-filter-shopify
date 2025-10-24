b59998bf2a9d7ac1ae38aa96fff982116173a7c2
jatin sharma <jatinsharma9871@gmail.com>
2025-10-24 17:08:02 +0530

    push

--- api/products.js
+++ api/products.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       const gqlQuery = {
         query: `
           query SearchProducts($cursor: String) {
-            products(first: 250, after: $cursor) {
+            products(first: 50, after: $cursor) {
               edges {
                 cursor
                 node {
