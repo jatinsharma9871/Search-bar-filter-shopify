238d9cbacfbb2971742beffb2833f8f98e8009e4
jatin sharma <jatinsharma9871@gmail.com>
2026-08-31 15:38:05 +0530

    comit

--- api/products.js
+++ api/products.js
@@ -111,8 +111,8 @@ export default async function handler(req, res) {
         }
       `,
       variables: {
-  search: `(title:*${escapedTerm}* OR vendor:*${escapedTerm}* OR product_type:*${escapedTerm}*) AND inventory_total:>0`,
-},
+        search: `(title:*${q}* OR vendor:*${q}* OR product_type:*${q}*)`,
+      },
     };
 
     let response = await fetch(
@@ -155,9 +155,9 @@ export default async function handler(req, res) {
     }
 
     const products =
-  result?.data?.products?.edges
-    ?.map((edge) => edge.node)
-    .filter((product) => product.totalInventory > 0) || [];
+      result?.data?.products?.edges?.map(
+        (edge) => edge.node
+      ) || [];
 
     return res.status(200).json({
       total: products.length,
